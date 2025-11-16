a4f86175d710001e04669fb53a67cdc3c334be36
santoriniosmarios59-max <santoriniosmarios59@gmail.com>
2025-11-17 00:35:37 +0200

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // allow global `prisma` across hot-reloads in dev
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-}
-
-export const prisma: PrismaClient =
-  global.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : [],
-  });
-
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
+const prisma = new PrismaClient();
+export { prisma };  // Εξαγωγή του prisma ως named export
