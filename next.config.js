32868b4ce401754013bfe5f133c23affd8710180
santoriniosmarios59-max <santoriniosmarios59@gmail.com>
2025-11-17 02:01:05 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.unsplash.com"],
+module.exports = {
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
   },
 };
-
-module.exports = nextConfig;
